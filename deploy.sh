2102ce1db58e56a54a5b7d8472f0068c4f1a1c72
dimsth <dimo35xf@gmail.com>
2024-07-01 13:03:49 +0300

    [refactor] change deploy.sh to push and build in dockerhub images

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-ZOOKEEPER_IMAGE="zookeeper-image:lastest"
-TEBIS_IMAGE="tebis-image:latest"
+ZOOKEEPER_IMAGE="dstath/tebis_test_bench:zookeeper"
 
 # Build Docker images
 echo "Building ZooKeeper image..."
 if docker build -t $ZOOKEEPER_IMAGE -f zookeeper/Dockerfile.zookeeper zookeeper/; then
-    docker push dstath/zookeeper-image:latest
+    docker push $ZOOKEEPER_IMAGE
     echo "ZooKeeper image built and pushed successfully."
 else
     echo "Failed to build ZooKeeper image."
@@ -15,7 +14,10 @@ fi
 
 echo "Building Tebis images..."
 if docker-compose -f tebis/docker-compose.yml up --build; then
-    docker push dstath/tebis-base:latest
+    docker push dstath/tebis_test_bench:tebis-base
+    docker push dstath/tebis_test_bench:tebis-zookeeper
+    docker push dstath/tebis_test_bench:tebis-app
+
     echo "Tebis Base image built and pushed successfully."
 else
     echo "Failed to build Tebis Base image."
